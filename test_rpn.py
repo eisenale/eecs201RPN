f67b5b09f27f34d19e27041e23ab47d9b5ab188e
eisenale <eisenale@umich.edu>
2019-11-05 00:44:12 -0500

    coverage updates

--- test_rpn.py
+++ test_rpn.py
@@ -3,7 +3,7 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-    ef test_add(self):
+    def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
     def test_subtract(self):
